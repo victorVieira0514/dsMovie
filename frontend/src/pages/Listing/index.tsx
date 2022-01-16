6877bcc5b6ec9f099dcee810ba97a4718e36327d
victorVieira0514 <victorv14052002@gmail.com>
2022-01-16 19:47:42 -0300

    movieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,34 @@
+import MovieCard from "../../components/MovieCard";
+import MovieScore from "../../components/MovieScore";
+import MovieStars from "../../components/MovieStars";
+import Pagination from "../../components/Pagination";
+
 function Listing() {
 
     return (
-        <h1>Página de listagem</h1>
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 
 }
