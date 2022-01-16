e00f4704f9516871a9af84061b467c8438e581e6
victorVieira0514 <victorv14052002@gmail.com>
2022-01-16 16:23:28 -0300

    Project created

---
+++ backend/dsMovie/src/test/java/com/devsuperior/dsMovie/DsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
